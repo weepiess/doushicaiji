022173fde1896c1ed81b0305bdb3f82d93280b01
PengC98 <694977655@qq.com>
2018-10-19 11:10:31 +0800

    for pull

--- src/auto_aim.cpp
+++ src/auto_aim.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
-#include "math.h"
+#include "pnp_solver.h"
 const String fileName = "/home/wyx/图片/pic-final/my_photo-199.jpg";
 const float AutoAim::max_offset_angle = 30;
 
@@ -11,6 +11,8 @@ AutoAim::AutoAim(int width, int height){
     this->IMG_HEIGHT = height;
     hasROI = false;
     resizeCount = 0;
+    lastPoint.x = 0;
+    lastPoint.y = 0;
 }
 
 AutoAim::~AutoAim(){}
@@ -28,6 +30,14 @@ bool cmp(RotatedRect &x, RotatedRect &y){
    return x.size.width>y.size.width;
 }
 
+bool AutoAim::checkBorder(){
+    if(IMG_WIDTH-1-rectROI.x<rectROI.width || IMG_HEIGHT-1-rectROI.y<rectROI.height || rectROI.x<0 || rectROI.y <0){
+        rectROI.x = rectROI.y = rectROI.width = rectROI.height = 0;
+        return false;
+    }
+    return true;
+}
+
 //两点距离
 float distPoint(Point2f center1, Point2f center2){
     return abs(center1.x-center2.x) + abs(center1.y-center2.y);
@@ -43,8 +53,7 @@ void AutoAim::setImage(Mat &img, Mat &mask, Color enemyColor){
     }
     split(mask,channel); 
     threshold(enemyColor==red ? (channel[2]-channel[0]) : (channel[0] - channel[2]), mask, 0, 255, THRESH_BINARY+THRESH_OTSU); //自适应阈值
-    Canny(mask, mask, 3, 9, 3);
-    //imshow("mask", mask);                                             
+    Canny(mask, mask, 3, 9, 3);                                       
 }
 
 void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
@@ -56,8 +65,7 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
     findContours(mask, contours, hierarcy, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
     RotatedRect temp;
     vector<RotatedRect> pre_lamps;
-    
-    //cout<<"contours.size: "<<contours.size()<<endl;
+
     float lastCenterX = 0, lastCenterY = 0;
     if(contours.size()<40){
         for(int i=0;i<contours.size();i++){
@@ -75,8 +83,6 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
                         //判断是否放入的条件 
                         if(max(temp.size.width, temp.size.height) < min(temp.size.width, temp.size.height)*1.2)
 			                continue;
-                        //if(temp.size.width<8||temp.size.height<10)
-                            //continue;
                         pre_lamps.push_back(temp);
                     }
                 }
@@ -89,10 +95,6 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
 
     //排序lamp找到满足比例条件的灯管
     sort(pre_lamps.begin(), pre_lamps.end(), cmp);
-
-    //for(int i=0; i<pre_lamps.size(); i++){
-    //    cout<<i<<" "<<pre_lamps[i].center.x<<" "<<pre_lamps[i].center.y<<endl;
-    //}
     
     //角度和高度的权重，角度更加重要，所以角度的偏差使得结果的值偏差更大
     int angle_diff_weight = 2;
@@ -102,16 +104,10 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
     //这里只跟右边三个进行比较
     int size = pre_lamps.size();
     vector<float> diff(size,0);
-    //float *diff = new float(size);
     vector<float> best_match_index(size,0);
-    //float *best_match_index = new float(size);
     for(int i=0; i<size; i++){
         diff[i] = 0x3f3f3f3f;
         best_match_index[i] = -1;
-
-        //cout<<"angle: "<<pre_lamps[i].angle;
-        //cout<<" length: "<<pre_lamps[i].size.height<<endl;
-        //cout<<"x: "<<pre_lamps[i].center.x<<endl;
     }
     
     //中心点间距离，平均高度，角度差，高度差
@@ -124,18 +120,14 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
         float theta_current=abs(current.angle);
         if(theta_current>95)
                     theta_current=-(180-theta_current);
+
         for(j=1; j<=4 || (i+j)<size; j++){
             //计算比例，筛选灯管
-            
             const RotatedRect &compare = pre_lamps[i+j];
-            //cout<<"i"<<i<<"i+j"<<i+j<<endl;
             float theta_compare=abs(compare.angle);
             if(theta_compare>95)
                     theta_compare=-(180-theta_compare);
             diff_angle = abs(theta_compare - theta_current);
-            //cout<<"disc: "<<theta_compare<<endl;
-            //cout<<"discu: "<<theta_current<<endl;
-            //cout<<"disang: "<<diff_angle<<endl;
             if(diff_angle > 15) continue;
             
             diff_height = abs(compare.size.height - current.size.height);
@@ -145,12 +137,9 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
             if(diff_y > 30) continue;
             
             dist = distPoint(compare.center, current.center);
-           
-            //cout<<"distt: "<<dist<<endl;
             if(dist>200 || dist<10) continue;
             avg_height = (compare.size.height + current.size.height) / 2;
             ratio = dist / avg_height;
-            //cout<<"ratio: "<<ratio<<endl;
             if(ratio > 5 || ratio < 1) continue;
             
             totalDiff = angle_diff_weight*diff_angle + height_diff_weight*diff_height+0.5*dist;
@@ -172,8 +161,6 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
     //遍历，将满足条件的灯管储存
     for(i=0; i<size; i++){
         int index = best_match_index[i];
-        //cout<<"i: "<<i<<" best_match_index: "<<index;
-        //cout<<" diff: "<<diff[i]<<endl;
         if(index == -1 || index <= i) continue;
         //找到匹配对
         if(i == best_match_index[index]){
@@ -181,10 +168,9 @@ void AutoAim::findLamp(Mat &mask, vector<RotatedRect> &lamps){
             lamps.push_back(pre_lamps[index]);
         }
     }
-    //cout<<"lamps counts: "<<lamps.size()/2<<endl;
 }
 
-void AutoAim::findBestArmor(vector<RotatedRect> &lamps, Point &bestCenter,Mat &best_lamps){
+void AutoAim::findBestArmor(Mat &src, vector<RotatedRect> &lamps, Point &bestCenter, vector<Point2f> &posAndSpeed,Mat &best_lamps, clock_t &start){
 
     int lowerIndex = -1;
     int lowerY = 0;
@@ -197,66 +183,74 @@ void AutoAim::findBestArmor(vector<RotatedRect> &lamps, Point &bestCenter,Mat &b
             lowerIndex = i;
         }
     }
-    
+
     if(lowerIndex == -1){
         if(hasROI){
             hasROI = resizeROI(rectROI, rectROI);
         }
     } else {
         if(!hasROI){
-            hasROI = true;
-            rectROI.x = lamps[lowerIndex].center.x - lamps[lowerIndex].size.width;
-            rectROI.y = lamps[lowerIndex].center.y - lamps[lowerIndex].size.height/2;
-            rectROI.width = abs(lamps[lowerIndex].center.x - lamps[lowerIndex+1].center.x) + lamps[lowerIndex].size.width + 
-                                lamps[lowerIndex+1].size.width;
-            rectROI.height =  lamps[lowerIndex].size.height + lamps[lowerIndex+1].size.height - fabs(lamps[lowerIndex].center.y - 
+            rectROI.x = lamps[lowerIndex].center.x - lamps[lowerIndex].size.width*2;
+            rectROI.y = lamps[lowerIndex].center.y - lamps[lowerIndex].size.height*2;
+            rectROI.width = abs(lamps[lowerIndex].center.x - lamps[lowerIndex+1].center.x) + 2*lamps[lowerIndex].size.width + 
+                                lamps[lowerIndex+1].size.width*2;
+            rectROI.height =  lamps[lowerIndex].size.height*2 + lamps[lowerIndex+1].size.height*2 - fabs(lamps[lowerIndex].center.y - 
                                 lamps[lowerIndex+1].center.y);                   
-            if(rectROI.height < 0 || rectROI.x < 0 || rectROI.y < 0){
-                rectROI.x = rectROI.y = rectROI.width = rectROI.height = 0;
-            }
-            bestCenter.x = (lamps[lowerIndex].center.x + lamps[lowerIndex+1].center.x)/2 + rectROI.x;
-            bestCenter.y = (lamps[lowerIndex].center.y + lamps[lowerIndex+1].center.y)/2 + rectROI.y;
-            best_lamps.at<float>(0)=lamps[lowerIndex].center.x;
-            best_lamps.at<float>(1)=lamps[lowerIndex].center.y;
-            best_lamps.at<float>(2)=lamps[lowerIndex+1].center.x;
-            best_lamps.at<float>(3)=lamps[lowerIndex].center.y;
+            if(!checkBorder()) return;
+            hasROI = true;
+            bestCenter.x = (lamps[lowerIndex].center.x + lamps[lowerIndex+1].center.x)/2;
+            bestCenter.y = (lamps[lowerIndex].center.y + lamps[lowerIndex+1].center.y)/2;
         } else {
             int height = (lamps[lowerIndex].size.height + lamps[lowerIndex+1].size.height)/2;
             if(height > 15){
                 bestCenter.x = (lamps[lowerIndex].center.x + lamps[lowerIndex+1].center.x)/2 + rectROI.x;
                 bestCenter.y = (lamps[lowerIndex].center.y + lamps[lowerIndex+1].center.y)/2 + rectROI.y;
-                best_lamps.at<float>(0)=lamps[lowerIndex].center.x;
-                best_lamps.at<float>(1)=lamps[lowerIndex].center.y;
-                best_lamps.at<float>(2)=lamps[lowerIndex].size.height;
-                best_lamps.at<float>(3)=lamps[lowerIndex].angle;
-                best_lamps.at<float>(4)=lamps[lowerIndex+1].center.x;
-                best_lamps.at<float>(5)=lamps[lowerIndex+1].center.y;
-                best_lamps.at<float>(6)=lamps[lowerIndex+1].size.height;
-                best_lamps.at<float>(7)=lamps[lowerIndex+1].angle;
+                if(IMG_WIDTH-1<bestCenter.x || IMG_HEIGHT-1<rectROI.y) bestCenter.x = -1;
             } else hasROI = false;
         }
     }
-    
+
+    if(bestCenter.x!=-1){
+        clock_t finish = clock();
+        double time = (double)(finish-start)/CLOCKS_PER_SEC;
+        putText(src, to_string(1.0/time), Point(10,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2, 8);
+        posAndSpeed.push_back(bestCenter);
+        Point speed;
+        speed.x = (bestCenter.x - lastPoint.x)/time*1000;
+        speed.y = (bestCenter.y - lastPoint.y)/time*1000;
+        posAndSpeed.push_back(speed);
+        lastPoint.x = bestCenter.x;
+        lastPoint.y = lastPoint.y;
+        best_lamps.at<float>(0)=lamps[lowerIndex].center.x;
+        best_lamps.at<float>(1)=lamps[lowerIndex].center.y;
+        best_lamps.at<float>(2)=lamps[lowerIndex].size.height;
+        best_lamps.at<float>(3)=lamps[lowerIndex].angle;
+        best_lamps.at<float>(4)=lamps[lowerIndex+1].center.x;
+        best_lamps.at<float>(5)=lamps[lowerIndex+1].center.y;
+        best_lamps.at<float>(6)=lamps[lowerIndex+1].size.height;
+        best_lamps.at<float>(7)=lamps[lowerIndex+1].angle;
+    }
 }
 
 bool AutoAim::resizeROI(Rect &origin, Rect &current){ 
     //记录调用resize的次数
-    if(resizeCount==2){
- 	    resizeCount=0;//清零
+    if(resizeCount==3){
+ 	    resizeCount=0;
         return false;
     }	
     
     //将ROI区域扩大
-    current.x=origin.x-origin.height/2;
-    current.y=origin.y-origin.width/2;
-    current.height=origin.height+origin.height;
-    current.width=origin.width+origin.width;
+    current.x=origin.x-origin.height/2 ;
+    current.y=origin.y-origin.width/2 ;
+    current.height=origin.height+origin.height ;
+    current.width=origin.width+origin.width ;
     
     //判断ROI是否越界
-    if(IMG_WIDTH-1-rectROI.x<rectROI.width||IMG_HEIGHT-1-rectROI.y<rectROI.height)
-        return false;
-    ++resizeCount;
-    return true;
+    bool isSuccess = checkBorder();
+    if(isSuccess){
+        ++resizeCount;
+    }
+    return isSuccess;
 }
 Point2f cal_x_y(int x,int y,int H,float angle,int is_up)
 {
@@ -299,8 +293,8 @@ Point2f cal_x_y(int x,int y,int H,float angle,int is_up)
 
 }
 void test(){
-    clock_t start, finish;
-    double time_tol;
+    clock_t start;
+    //double time_tol;
     VideoCapture cap(1);
     if(!cap.isOpened()) return;
     cap.set(CAP_PROP_FRAME_WIDTH, 1280);
@@ -308,22 +302,57 @@ void test(){
     Mat src;
 
     AutoAim autoAim(1280, 720);
-
-    
-    Mat measurement = Mat::zeros(4, 1, CV_32F);
-    Mat best_lamps = Mat::zeros(8, 1, CV_32F);  
+    Mat best_lamps = Mat::zeros(8, 1, CV_32F); 
+     Mat measurement = Mat::zeros(4, 1, CV_32F); 
     Mat state(4, 1, CV_32F);
+    Mat processNoise(2, 1, CV_32F);
+    float dt=1/40;
+    autoAim.kf.transitionMatrix=(Mat_<float>(4, 4) <<   
+            1,0,dt,0,   
             0,1,0,dt,   
+            0,0,1,0,   
+            0,0,0,1 );
+    autoAim.kf.measurementMatrix=(Mat_<float>(4, 4) <<   
+            1,0,0,0,   
             0,1,0,0,   
+            0,0,1,0,   
+            0,0,0,1 );  
+    autoAim.kf.measurementNoiseCov=(Mat_<float>(4, 4) <<   
+            2000,0,0,0,   
             0,2000,0,0,   
             0,0,10000,0,   
             0,0,0,10000 );
+    autoAim.kf.init(4,10000,0);
+    Point bestCenter;
+    
     while(1){
+        PNPSolver pnpsolver(1242.230744,1245.580702,704.384156
+    ,368.265589,0.002554, 0.140210, -0.001525, 0.004035, 0.000000);
+        start=clock();
         cap>>src;
+        if(src.empty()) break;
+        Mat mask;
+        autoAim.setImage(src, mask, autoAim.red);
 
-        pnpsolver.Points3D.push_back(cv::Point3f(135, 0, 0));   //P3
+        vector<RotatedRect> lamps;
+        autoAim.findLamp(mask, lamps);
 
+        bestCenter.x = -1;
+        vector<Point2f> posAndSpeed;
+        autoAim.findBestArmor(src, lamps, bestCenter,posAndSpeed, best_lamps,start);
+
+        rectangle(src, autoAim.rectROI, Scalar(255,0,0), 7);
+        if(bestCenter.x!=-1) 
+        {
+            pnpsolver.Points3D.push_back(cv::Point3f(0, 0, 0));     //P1三维坐标的单位是毫米
             pnpsolver.Points3D.push_back(cv::Point3f(0, 55, 0));   //P2
+            pnpsolver.Points3D.push_back(cv::Point3f(135, 0, 0));   //P3
+        //p4psolver.Points3D.push_back(cv::Point3f(150, 200, 0));   //P4
+            pnpsolver.Points3D.push_back(cv::Point3f(135, 55, 0));
+            int xc1=best_lamps.at<float>(0);//first center x
+            int yc1=best_lamps.at<float>(1);//first center y
+            int h1=best_lamps.at<float>(2);//first hight   
+            int a1=best_lamps.at<float>(3);//first angle
             int xc2=best_lamps.at<float>(4);//第二个灯条 x
             int yc2=best_lamps.at<float>(5);//第二个灯条 y
             int h2=best_lamps.at<float>(6);   //第二个灯条 hight
@@ -341,22 +370,31 @@ void test(){
                 pnpsolver.Points2D.push_back(cal_x_y(xc1,yc1,h1,a1,1));//P3
                 pnpsolver.Points2D.push_back(cal_x_y(xc1,yc1,h1,a1,0));//P4
             }
-            if (pnpsolver.Solve(PNPSolver::METHOD::CV_P3P) == 0)
-                cout << "test2:CV_P3P方法:  相机位姿→" << "Oc坐标=" << pnpsolver.Position_OcInW << "    相机旋转=" << p4psolver.Theta_W2C << endl;
+            if (pnpsolver.Solve(PNPSolver::METHOD::CV_P3P_) == 0)
+                 cout << "test2:CV_P3P方法:  相机位姿→" << "Oc坐标=" << pnpsolver.Position_OcInW << "    相机旋转=" << pnpsolver.Theta_W2C << endl;
             Mat Predict =autoAim.kf.predict();
-            for(int i=0;i<4;i++)
-            {
-                state.at<float>(i)=Predict.at<float>(i);
-            }
+            cout<<"1"<<endl;
             measurement.at<float>(0)= (float)posAndSpeed[0].x;
             measurement.at<float>(1) = (float)posAndSpeed[0].y;  
             measurement.at<float>(2)= (float)posAndSpeed[1].x;  
             measurement.at<float>(3) = (float)posAndSpeed[1].y;
-            autoAim.kf.correct(measurement);     
-            circle(src, bestCenter, 20, Scalar(255,255,255), 5);  
+            cout<<"2"<<endl;
+            autoAim.kf.correct(measurement);
+            circle(src,Point2f(Predict.at<float>(0),Predict.at<float>(1)),20,Scalar(255,255,0),5);
+            circle(src, bestCenter, 20, Scalar(255,255,255), 5);
         }
-        finish = clock();
-        //cout<<time_tol<<endl;
-        time_tol = (double)(finish - start)/ CLOCKS_PER_SEC;
-        imshow("src", sryou
+        //finish = clock();
+        //time_tol = (double)(finish - start)/ CLOCKS_PER_SEC;
+        //putText(src, to_string(1.0/time_tol), Point(10,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2, 8);
+        imshow("src", src);
+        char c = waitKey(1);
+        if((char)c == 27) break;
+    }
+    cap.release();
+}
+
+int main(int argc, char const *argv[]){
+    
+    test();
+    return 0;
 }
